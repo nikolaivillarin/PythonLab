b1feec70701cca72af2d2ba653cb032d32338c71
Nikolai Villarin <nikolaivillarin@grandandtoy.com>
2019-03-31 21:38:44 -0400

    checkpoint

---
+++ fetch.py
@@ -0,0 +1,129 @@
+import os
+import tarfile
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+from six.moves import urllib
+from zlib import crc32
+from sklearn.model_selection import StratifiedShuffleSplit
+from pandas.plotting import scatter_matrix
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml/master/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+def fetch_housing_data(housing_url = HOUSING_URL, housing_path = HOUSING_PATH):
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+
+    urllib.request.urlretrieve(housing_url, tgz_path)
+
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path = housing_path)
+    housing_tgz.close()
+
+def load_housing_data(housing_path = HOUSING_PATH):
+    csv_path = os.path.join(housing_path, "housing.csv")
+
+    return pd.read_csv(csv_path)
+
+def test_set_check(identifier, test_ratio):
+    return crc32(np.int64(identifier)) & 0xffffffff < test_ratio * 2**32
+
+def split_train_test(data, test_ratio):
+    shuffled_indices = np.random.permutation(len(data))
+
+    test_set_size = int(len(data) * test_ratio)
+    test_indices = shuffled_indices[:test_set_size]
+    train_indices = shuffled_indices[test_set_size:]
+
+    return data.iloc[train_indices], data.iloc[test_indices]
+
+def split_train_test_by_id(data, test_ratio, id_column):
+    ids = data[id_column]
+
+    in_test_set = ids.apply(lambda id_: test_set_check(id_, test_ratio))
+
+    return data.loc[~in_test_set], data.loc[in_test_set]
+
+#region Retrieves New Data
+fetch_housing_data()
+#endregion
+
+#region Stores Housing Data
+housing = load_housing_data()
+#endregion
+
+#region Examples of show attributes and gaining insight of data
+housing.head()
+housing.info()
+housing["ocean_proximity"].value_counts()
+housing.describe()
+#endregion
+
+#region Example of splitting test data
+housing_with_id = housing.reset_index() # adds an `index` column
+train_set, test_set = split_train_test_by_id(housing_with_id, 0.2, "index")
+#endregion
+
+#region Example of cutting Data
+
+housing["income_cat"] = pd.cut(housing["median_income"],
+    bins = [0., 1.5, 3.0, 4.5, 6., np.inf],
+    labels = [1, 2, 3, 4, 5])
+
+housing["income_cat"].hist()
+
+split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
+
+for train_index, test_index in split.split(housing, housing["income_cat"]):
+    strat_train_set = housing.loc[train_index]
+    strat_test_set = housing.loc[test_index]
+
+strat_test_set["income_cat"].value_counts() / len(strat_test_set)
+
+#endregion
+
+#region Undoing Example of Cutting Data
+for set_ in (strat_train_set, strat_test_set):
+    set_.drop("income_cat", axis=1, inplace=True)
+#endregion
+
+#region Discover and Visualize the Data to Gain Insights
+
+# Copy the training set
+housing = strat_train_set.copy()
+
+# Since there is geographical information (latitude and longitude),
+# it is a good idea to create a scatterplot of all districts
+# Alpha Property - Setting this makes it much easier to visualize the places that
+#                - have hight density
+housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.1)
+
+# cmap (Color Map) - called jet, which ranges from blue (low values) to red (high values)
+# Option s - radius of each circle - in our case we are using district's population
+# Option c - color - in our case to represent the price
+housing.plot(kind="scatter", x="longitude", y="latitude", alpha=0.4,
+    s=housing["population"]/100, label="population", figsize=(10,7),
+    c="median_house_value", cmap=plt.get_cmap("jet"), colorbar=True
+)
+plt.legend()
+
+# Looking for Correlations
+corr_matrix = housing.corr()
+corr_matrix["median_house_value"].sort_values(ascending=False)
+
+# Scatter_Matrix function
+attributes = ["median_house_value", "median_income", "total_rooms",
+    "housing_median_age"]
+
+scatter_matrix(housing[attributes], figsize=(12, 8))
+
+housing.plot(kind="scatter", x="median_income", y="median_house_value", alpha=0.1)
+
+# 
+
+#endregion
