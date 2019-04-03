d597db499f4ae5d3b9d0ae206a255dcd23ad9eb7
Nikolai Villarin <nikol3x@users.noreply.github.com>
2019-04-03 17:19:04 -0400

    Started customTransforms

---
+++ Chapt2/customTransforms.py
@@ -0,0 +1,12 @@
+import Chapt2.cuttingData
+
+from sklearn.base import BaseEstimator, TransformerMixin
+
+rooms_ix, bedrooms_ix, population_ix, households_ix = 3, 4, 5, 6
+
+class CombinedAttributesAdder(BaseEstimator, TransformerMixin):
+    def __init__(self, add_bedrooms_per_room = True): # no *args or **kargs
+        self.add_bedrooms_per_room = add_bedrooms_per_room
+
+    def fit(self, X, y=None):
+        return self # nothing else to do
