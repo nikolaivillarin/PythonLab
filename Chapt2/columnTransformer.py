e8d283e10496e104d27052fd25615821c91449b8
Nikolai Villarin <nikolaivillarin@grandandtoy.com>
2019-04-06 16:17:24 -0400

    ColumnTransformer example

---
+++ Chapt2/columnTransformer.py
@@ -0,0 +1,14 @@
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+
+num_attribs = list(housing_num)
+cat_attribs = ["ocean_proximity"]
+
+full_pipeline = ColumnTransformer([
+    ("num", num_pipeline, num_attribs),
+    ("cat", OneHotEncoder(), cat_attribs)
+])
+
+housing_prepared = full_pipeline.fit_transform(housing)
+
+housing_prepared
--- Chapt2/pipelines.py
+++ Chapt2/pipelines.py
@@ -1,12 +1,13 @@
 import Chapt2.customTransforms
+
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScalar
+from sklearn.preprocessing import StandardScaler
 from sklearn.impute import SimpleImputer
 
 num_pipeline = Pipeline([
     ('imputer', SimpleImputer(strategy="median")),
-    ('attribs_adder', CombinedAttributeAdder()),
-    ('std_scalar', StandardScalar())
+    ('attribs_adder', CombinedAttributesAdder()),
+    ('std_scalar', StandardScaler())
 ])
 
 housing_num_tr = num_pipeline.fit_transform(housing_num)
