5e6bade5a8e3085d3e577be3b74e7d3f44c43354
Nikolai Villarin <nikol3x@users.noreply.github.com>
2019-04-12 16:54:42 -0400

    Completed training on Basics of Pandas and NumPy

--- pandas/datastructures.py
+++ pandas/datastructures.py
@@ -9,7 +9,8 @@ series1
 series2 = pd.Series(np.random.randn(5), index=['a', 'b', 'c', 'd', 'e'])
 series2
 
-npArray = np.array([1,2,3])
+npArray = np.array([1, 2, 5, 7, 8])
+npArray[1:3] = -1
 npArray
 
 npOnes = np.ones((3, 4), dtype=np.int16)
@@ -17,3 +18,39 @@ npOnes
 
 npFull = np.full((3, 4), 0.11)
 npFull
+
+npArrange = np.arange(10, 30, 2)
+npArrange
+
+npLinspace = np.linspace(0, 6, 3)
+npLinspace
+
+npRandom = np.random.rand(2, 3)
+npRandom
+
+npEmpty = np.empty((2, 3))
+npEmpty
+
+npSliceOriginal = np.array([1,2,5,7,8])
+npSlice = npSliceOriginal[1:5]
+npSlice[1] = 1000
+npSlice
+
+npSliceCopy = npSliceOriginal[1:5].copy()
+npSliceCopy[1] = 3000
+npSliceCopy
+npSlice
+
+npBooleanIndexing = np.arange(12).reshape(3,4)
+npBooleanIndexing
+
+rows_on = np.array([True, False, True])
+rows_on
+
+npBooleanIndexing[rows_on, :]
+
+people_dict = {
+    "weight": pd.Series([68, 83, 112], index=["alice", "bob", "charles"]),
+    "birthyear": pd.Series([1984, 1985, 1992], index=["bob", "alice", "charles"], name="year"),
+    "children": pd.Series([0,3], index=["charles", "bob"])
+}
