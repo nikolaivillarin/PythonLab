7bbee5ff45197802ffca05d84ba9d23c7f4097e2
Nikolai Villarin <nikol3x@users.noreply.github.com>
2019-04-15 17:33:18 -0400

    Something before Iryna intrupted me...

---
+++ Chapt2/gridSearchCV.py
@@ -0,0 +1,7 @@
+from sklearn.model_selection import GridSearchCV
+
+param_grid = [
+    {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
+    {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]}
+]
+
