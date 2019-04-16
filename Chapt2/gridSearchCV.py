b85621039825cc6f0a786828efe06e77fc3ff272
Nikolai Villarin <nikol3x@users.noreply.github.com>
2019-04-16 17:21:12 -0400

    Completed GridSearchCV

--- Chapt2/gridSearchCV.py
+++ Chapt2/gridSearchCV.py
@@ -1,7 +1,19 @@
 from sklearn.model_selection import GridSearchCV
+from sklearn.ensemble import RandomForestRegressor
+
+from Chapt2.trainingModel import *
 
 param_grid = [
     {'n_estimators': [3, 10, 30], 'max_features': [2, 4, 6, 8]},
     {'bootstrap': [False], 'n_estimators': [3, 10], 'max_features': [2, 3, 4]}
 ]
 
+forest_reg = RandomForestRegressor()
+
+grid_search = GridSearchCV(forest_reg, param_grid, cv=5,
+    scoring="neg_mean_squared_error",
+    return_train_score=True)
+
+grid_search.fit(housing_prepared, housing_labels)
+
+grid_search.best_params_
