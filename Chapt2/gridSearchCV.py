c57ad84e8d513f3bb0e8b577f73144ed67364a8d
Nikolai Villarin <nikolaivillarin@grandandtoy.com>
2019-04-18 16:59:40 -0400

    Start of chapter 3

--- Chapt2/gridSearchCV.py
+++ Chapt2/gridSearchCV.py
@@ -17,3 +17,28 @@ grid_search = GridSearchCV(forest_reg, param_grid, cv=5,
 grid_search.fit(housing_prepared, housing_labels)
 
 grid_search.best_params_
+
+feature_importances = grid_search.best_estimator_.feature_importances_
+
+# Display feature importance scores next to their corresponding attribute names:
+extra_attribs = ["rooms_per_hhold", "pop_per_hhold", "bedrooms_per_room"]
+
+cat_encoder = full_pipeline.named_transformers_["cat"]
+cat_one_hot_attribs = list(cat_encoder.categories_[0])
+attributes = num_attribs + extra_attribs + cat_one_hot_attribs
+
+sorted(zip(feature_importances, attributes), reverse=True)
+
+
+# --== Evaluate Your System on the Test Set ==--
+final_model = grid_search.best_estimator_
+
+x_test = strat_test_set.drop("median_house_value", axis=1)
+y_test = strat_test_set["median_house_value"].copy()
+
+x_test_prepared = full_pipeline.transform(x_test)
+
+final_predictions = final_model.predict(x_test_prepared)
+
+final_mse = mean_squared_error(y_test, final_predictions)
+final_rmse = np.sqrt(final_mse)
