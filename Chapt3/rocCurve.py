2785593171b8e524df1de7aedbf186d1241ddd3f
Nikolai Villarin <nikol3x@users.noreply.github.com>
2019-04-22 17:20:09 -0400

    Started ROC curve tutorial

---
+++ Chapt3/rocCurve.py
@@ -0,0 +1,15 @@
+from sklearn.metrics import roc_curve
+
+from Chapt3.sgdClassifier import *
+
+fpr, tpr, thresholds = roc_curve(y_train_5, y_scores)
+
+def plot_roc_curve(fpr, tpr, label=None):
+    plt.plot(fpr, tpr, linewidth=2, label=label)
+    plt.plot([0, 1], [0, 1], 'k--')
+    plt.axis([0, 1, 0, 1])
+    plt.xlabel('False Positive Rate')
+    plt.ylabel('True Positive Rate')
+
+plot_roc_curve(fpr, tpr)
+plot.show()
