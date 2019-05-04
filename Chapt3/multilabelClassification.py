4f6f48faed62deb8fb4a360486a712c931586e14
Nikolai Villarin <nikolaivillarin@grandandtoy.com>
2019-05-04 14:09:46 -0400

    Scored the Multilabel Classification

---
+++ Chapt3/multilabelClassification.py
@@ -0,0 +1,13 @@
+from Chapt3.errorAnalysis import *
+
+# Multilabel Classification
+from sklearn.neighbors import KNeighborsClassifier
+
+y_train_large = (y_train >= 7)
+y_train_odd = (y_train % 2 == 1)
+y_multilabel = np.c_[y_train_large, y_train_odd]
+
+knn_clf = KNeighborsClassifier()
+knn_clf.fit(X_train, y_multilabel)
+
+knn_clf.predict([some_digit])
