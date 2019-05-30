103f8557fdd80f58f0201767c871d9da4c9b9af3
Nikolai Villarin <nikolaivillarin@grandandtoy.com>
2019-05-30 14:04:40 -0700

    Reinstalled windows environment

--- envwin/Scripts/Activate.ps1
+++ envwin/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\Administrator\Desktop\20190403_Python\envwin"
+$env:VIRTUAL_ENV="C:\Users\Administrator\Desktop\20190630_Python\envwin"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
