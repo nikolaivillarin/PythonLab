cc37513a911d862d45aa2bc3d7e0cf7389ccab16
Nikolai Villarin <nikolaivillarin@grandandtoy.com>
2019-05-30 14:19:34 -0700

    Complete SGD tutorial

---
+++ Chapt4/stochasticGradientDescent.py
@@ -0,0 +1,23 @@
+from __future__ import division, print_function, unicode_literals
+from Chapt4.linearRegression import *
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+m = 100
+n_epochs = 50
+t0, t1 = 5, 50 # learning schedule hyperparameters
+
+def learning_schedule(t):
+    return t0 / (t + t1)
+
+theta = np.random.randn(2, 1) # random initialization
+
+for epoch in range(n_epochs):
+    for i in range(m):
+        random_index = np.random.randint(m)
+        xi = X_b[random_index:random_index + 1]
+        yi = y[random_index:random_index + 1]
+        gradients = 2 * xi.T.dot(xi.dot(theta) - yi)
+        eta = learning_schedule(epoch * m + i)
+        theta = theta - eta * gradients
